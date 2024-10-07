578789bb76dec27bba955b0952ae414069fc7299
llsiddharthll <siddharthgreat443@gmail.com>
2024-10-07 17:27:48 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'video-pattern': "url('https://iko.themegenix.net/blockchain/wp-content/uploads/2024/04/video_bg.webp')",
+      }
+    },
   },
   plugins: [],
 }
